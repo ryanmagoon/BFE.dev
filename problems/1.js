53942553a436ce4368910bd8db54f3214b36e0da
Ryan Magoon <mr.magoon5@gmail.com>
2021-05-21 01:02:57 -0400

    update curry to optimal solution

--- problems/1.js
+++ problems/1.js
@@ -2,14 +2,10 @@
  * @param { (...args: any[]) => any } fn
  * @returns { (...args: any[]) => any }
  */
- function curry(fn) {
-    let finalArgs = []
-    const curriedFunction = (...args) => {
-      if (finalArgs.length + args.length >= fn.length) {
-        return fn(...finalArgs, ...args)
-      }
-      finalArgs.push(...args)
-      return curriedFunction
-    }
-    return curriedFunction
-  }
+const curry = (fn) => {
+  const curriedFn = (...args) =>
+    args.length >= fn.length
+      ? fn(...args)
+      : (...moreArgs) => curriedFn(...args, ...moreArgs)
+  return curriedFn
+}
